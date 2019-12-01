acb471e0d109b1bcae5f0f6ed62efbe3a2d5a3ab
khalid7487 <khalid15-7487@diu.edu.bd>
2019-12-01 11:38:32 +0600

    update secondary level cache

--- HibernateChaceTest/src/cacheExample/ChaceTest.java
+++ HibernateChaceTest/src/cacheExample/ChaceTest.java
@@ -6,6 +6,7 @@
 package cacheExample;
 
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -14,19 +15,24 @@ import org.hibernate.SessionFactory;
  * @author KHALID
  */
 public class ChaceTest {
+
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Employee emp=(Employee) session.get(Employee.class, 1);
-       // emp.setEmp_name("Update Name");         
+//        Employee emp=(Employee) session.get(Employee.class, 1);
+      //  Employee emp = (Employee) session.createQuery("from Employee");
+        List<Employee> emp = (List<Employee>) session.createQuery("from Employee").setCacheable(true).list();
+        // emp.setEmp_name("Update Name");         
         session.getTransaction().commit();
         session.close();
         Session session2 = sessionFactory.openSession();
         session2.beginTransaction();
-        Employee emp2=(Employee) session2.get(Employee.class, 1);
+        List<Employee> emp2 = (List<Employee>) session2.createQuery("from Employee").setCacheable(true).list();
+//        Employee emp2 = (Employee) session2.createQuery("from Employee");
+//        Employee emp2=(Employee) session2.get(Employee.class, 1);
         session2.getTransaction().commit();
         session2.close();
     }
-    
+
 }
